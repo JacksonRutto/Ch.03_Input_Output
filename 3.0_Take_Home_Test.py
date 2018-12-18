0f6297c3249331bee90284a4471b2bcef6dba51d
JacksonRutto <ruttojac@urbandaleschools.com>
2018-12-18 13:36:23 -0600

    end of tuesday

--- 3.0_Take_Home_Test.py
+++ 3.0_Take_Home_Test.py
@@ -6,13 +6,12 @@ but I will definitely not ask another person except the instructor. Signed: Jack
 
 1. Write a line of code that will print your name.
 '''
-print("Jackson Rutto")
+print ("Jackson Rutto")
 
 '''
 2. How do you enter a comment in a program?
-
 '''
-#Use a hashtag or for a multiple line comment use three apostrophes on each side
+#Use a hashtag to comment
 
 '''
 3. What is the ouput of the following lines of code?
@@ -20,17 +19,24 @@ print (2/3)
 print (2//3)
 '''
 print (2/3)
+#This prints 0.6666666
+
+print (2//3)
+#This prints 0
+
 '''
 4. Write a line of code that creates a variable called pi and sets it to an
 appropriate value.
-
 '''
+pi = 3.14
 
 '''
 5. Why does this code not work?
 A=22
 print(a)
 '''
+
+
 
 '''
 6. All of the variable names below can be used. But which of these is the best?
@@ -42,6 +48,7 @@ area
 areaOfRectangle
 AreaOfRectangle
 '''
+
 
 '''
 7. Which of these variables names are not allowed in Python?
@@ -57,12 +64,15 @@ accountNumber
 account#
 '''
 
+
+
 '''
 8. Why does this code not work?
-
 print(a)
 a=45
 '''
+
+
 
 '''
 9. Write a line of code that will ask the user for the length of a square's
@@ -70,14 +80,19 @@ side and store the result in a variable. Make sure to convert the value
 to an integer.
 '''
 
+
 '''
 10. Write a line of code that prints the area of the square, using the
 number the user typed in that you stored in question 9.
 '''
 
+
+
 '''11. Do the same as in questions 9 and 10, but with the formula for the
 area of an ellipse. Area=pi*a*b where a and b are the lengths of the major radii.
 '''
+
+
 
 '''
 12. Do the same as in questions 9 and 10, but with a formula to find
@@ -85,29 +100,30 @@ the pressure of a gas. PV=nRT where n is the number of moles, T is the absolute 
 volume, and R is the gas constant 8.3144.
 '''
 
+
 '''
 13. Explain the mistake in this code:
-
 pi = float(3.14)
 '''
 
+
+
 '''
 14. Assume radius has been defined. This code runs, but isn't very good. Explain the mistake in the following code:
-
 x=3.14
 pi=x
 area=pi*radius**2
 '''
 
+
 '''
 Assuming x and y are already defined, this code runs. But
 something isn't quite right. Explain the mistake in the following code:
-
 a=((x)*(y))
 '''
 
+
 '''
 16. Explain the mistake in the following code:
-
 radius = input(float("Enter the radius:"))
 '''
